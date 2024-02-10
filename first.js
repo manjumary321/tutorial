44f85ac3c71a45e4c54062b27acdf472f3f25a99
manjumary321 <106730320+manjumary321@users.noreply.github.com>
2024-02-10 15:16:32 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,173 @@
+// //variable dat type
+
+// console.log("Welcome to Apna College!");    //output   Welcome to Apna College!
+// console.log(" Apna Deepak");                //output   Apna Deepak
+// console.log("I love Javascript!");         //output   I love Javascript!  
+// console.log(' Javascript!')                //output   Javascript!
+
+//----------------end variable dat type
+
+
+
+
+// //variable in js,,Dynamic Type
+
+// fullName = "Tony Stark";
+// age = 24;
+// price = 99.99;
+// radius = 14;
+// x = null;
+// y = undefined;
+// isFollow = true;    // true/false
+
+// fullName = 25;
+// // console.log("fullName");  //output   Tony Stark
+// // console.log( age);    //output   24
+// // console.log(price);   //output   99.99
+// // console.log(x);       //output   null
+// // console.log(y);       //output  undefined
+// // console.log(isFollow);  //output   true
+// console.log(fullName);   //output   25
+
+//--
+
+//  //variable in js,   case sensitive
+
+//  fullname = "beena jose";
+//  fullName = "navy don";
+
+//  console.log(fullname);  //output   beena jose
+//  console.log(fullName);   //output  navy don
+
+
+//  //   ---
+
+//  fullName = "beena jose";
+//  _fullName = "beena jose";
+//  $fullName = "beena jose";
+//  fullName = "beena jose";
+
+//  console.log(fullName); 
+
+//  //---
+// console - "beena jose "
+//  console.log(console)  //output  error         not small letter
+
+//  Console - "beena jose "
+//  console.log(Console)  //output  beena jose        capital letter
+
+
+ //  ------------  end variable in js
+
+
+ // Let, var, const
+
+ // let
+//  let name = "Tony shark";
+//  console.log(name);        //output  Tony shark   
+
+//  let fullName = "Tony shark";
+//  console.log(fullName);        //output  Tony shark 
+
+//  let fullName = "Tony shark";
+//  let age = 24;
+//  let totalPrice = 1000;
+//  console.log(fullName,age,totalPrice);        //output  Tony shark 24 1000
+
+ //--- end let
+
+ // var
+//  var fullName = "Tony shark";
+//  var age = 24;
+//  var totalPrice = 1000;
+//  console.log(fullName,age,totalPrice);        //output  Tony shark 24 1000
+
+
+
+// var age = 24;
+// var age = 59;
+// var age = 87;
+// console.log(age);    //output  87
+
+
+// var age = 24;
+// age = 59;
+// age = 37;
+// console.log(age);    //output  37
+
+// let age = 24;
+//  age = 59;
+//  age = 97;
+// console.log(age);    //output  97
+
+////   --end let
+
+ //// const
+
+
+// const age = 24;
+//  age = 59;
+//  age = 97;
+// console.log(age);    //output  first.js:105 Uncaught TypeError: Assignment to constant variable.
+
+
+// const PI = 3.14;
+//  console.log(PI);    //output  3.14
+
+
+// let  a;
+// console.log(a);    //output  undefined
+
+
+// let  a = 10;
+// console.log(a);    //output  10
+
+
+// const a;
+// console.log(a);  //output  Uncaught SyntaxError: Missing initializer in const declaration (at first.js:126:7)
+
+
+// const a = 10;
+// console.log(a);  //output  10
+
+//// end.....
+
+// //block---
+
+// {
+//     let a =5;
+//     console.log(a);
+// }
+// console.log(a);    // output 5,   first.js:140 Uncaught ReferenceError: a is not defined at first.js:140:13
+
+// ...
+
+// {
+//     let a =5;
+//     let a =10; //Cannot redeclare block-scoped variable 'a'.
+//     console.log(a);    // output 5,  Uncaught SyntaxError: Identifier 'a' has already been declared 
+// }
+
+//...
+
+// {
+//     let a =5;
+//     console.log(a);    // output 5, 
+// }
+// {
+//     let a =10;
+//     console.log(a);    // output 10, 
+// }
+
+//...
+
+
+//  // end ..... Let,const,var
+
+
+
+////  Data type in javascript
+
+let age = 24;
+
+
